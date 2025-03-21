3d40932ae65ed5926c2a7fb0392ec366ff0d876e
NandhiniRam <nandhini051@outlook.com>
2025-03-21 11:22:32 -0600

    frame and layer header files complete

--- frame.h
+++ frame.h
@@ -1,10 +1,42 @@
 #ifndef FRAME_H
 #define FRAME_H
 
+#include "layer.h"
+#include <vector>
+
+using std::vector;
+
 class Frame
 {
 public:
-    Frame();
+    Frame(unsigned int width, unsigned int height);
+    ~Frame();
+    bool operator==(const Frame& other) const;
+
+    Pixel** getLayeredImage() const;
+    Pixel** getRenderedImage() const;
+
+    void selectLayer(unsigned int index);
+    void deleteLayer();
+    void addLayer(bool copyPrevious);
+    void resize(unsigned int newSize);
+
+    void paintPixels(unsigned int corner1, unsigned int corner2, const Pixel& color);
+    void bucketFill(unsigned int x, unsigned int y, const Pixel& color);
+    Pixel getPixel(unsigned int x, unsigned int y) const;
+    void reflectVertical();
+    void reflectHorizontal();
+    void rotate90();
+
+private:
+    std::vector<Layer> layers;
+    unsigned int activeLayer;
+    Pixel** layeredImage;
+    unsigned int width;
+    unsigned int height;
+
+    void rerenderImage();
+    void deSelectCurrentLayer();
 };
 
 #endif // FRAME_H
