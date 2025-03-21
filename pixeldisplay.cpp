5a24cfc30f93c5462e6964773bc563dc0285ebc3
jakeheairld <jakeheairld1@gmail.com>
2025-03-21 12:25:22 -0600

    pixel display header file

--- pixeldisplay.cpp
+++ pixeldisplay.cpp
@@ -1,5 +1,13 @@
 #include "pixeldisplay.h"
+#include <QPoint>
 
 PixelDisplay::PixelDisplay(QWidget *parent)
-    : QWidget{parent}
+    : QWidget{parent}, width(32), height(32) // example 32x32 pixel resolution
 {}
+
+QPoint PixelDisplay::mapPixelCoordinateToUICoordinate(unsigned int pixelX, unsigned int pixelY) {
+    unsigned int uiX = pixelX * (800 / pixelResolutionWidth); // 800 is the widget width
+    unsigned int uiY = pixelY * (800 / pixelResolutionHeight); // 800 is the widget height
+    return QPoint(uiX, uiY);
+}
+
--- pixeldisplay.h
+++ pixeldisplay.h
@@ -1,13 +1,41 @@
+/*
+The header file for pixel display.
+
+A class which represents the pixels displayed for the frames.
+
+By Joel Rodriguez, Jacob Anderson,
+Adharsh Ramakrishnan, Nandhini Ramanathan
+Jake Heairld & Joseph Hamilton
+
+March 21, 2025
+*/
 #ifndef PIXELDISPLAY_H
 #define PIXELDISPLAY_H
 
 #include <QWidget>
+#include "pixel.h"
 
 class PixelDisplay : public QWidget
 {
     Q_OBJECT
+
 public:
+    /**
+     * Creates a new pixel display object
+     * @param parent - the parent of the model
+     */
     explicit PixelDisplay(QWidget *parent = nullptr);
+
+private:
+    unsigned int pixelResolutionWidth;
+    unsigned int pixelResolutionHeight;
+
+    QPoint mapPixelCoordinateToUICoordinate(unsigned int pixelX, unsigned int pixelY);
+
+    void drawPixel(unsigned int pixelX, unsigned int pixelY, Pixel currentPixel);
+
+public slots:
+    void updateDrawnImage(Pixel[][]);
 
 signals:
 };
