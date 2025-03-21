74e8cff312d5b551b8aabbe5b8748703aa26619e
jacobanderson <jacobAnderson931@gmail.com>
2025-03-21 10:54:37 -0600

    pixel class setup

--- pixel.h
+++ pixel.h
@@ -5,6 +5,12 @@ class Pixel
 {
 public:
     Pixel();
+    Pixel(unsigned char red, unsigned char green, unsigned char blue, unsigned char alpha);
+    bool operator==(const Pixel& other) const;
+    unsigned char red;
+    unsigned char green;
+    unsigned char blue;
+    unsigned char alpha;
 };
 
 #endif // PIXEL_H
