ede15cef8796b5a0609fc9c53ca34aff47b56a83
jacobanderson <jacobAnderson931@gmail.com>
2025-03-21 10:39:15 -0600

    first commit

---
+++ mainmodel.h
@@ -0,0 +1,13 @@
+#ifndef MAINMODEL_H
+#define MAINMODEL_H
+
+class MainModel : public QObject
+{
+    Q_OBJECT
+public:
+    explicit MainModel(QObject *parent = nullptr);
+
+signals:
+};
+
+#endif // MAINMODEL_H
