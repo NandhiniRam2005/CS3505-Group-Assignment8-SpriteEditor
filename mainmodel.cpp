973ea447337ad59b0dfd2da6ca4c09462728a82e
jacobanderson <jacobAnderson931@gmail.com>
2025-03-21 16:36:20 -0600

    basic slot functionality in main model

--- mainmodel.cpp
+++ mainmodel.cpp
@@ -4,6 +4,10 @@ MainModel::MainModel(QObject *parent)
     : QObject{parent}
 {}
 
+//TODO: load / save JSON methods
+// mouseHovered / make grid transparent where mouse is hovering
+// send animation frames on a timer
+// attach signals / slots
 void MainModel::loadJSON(QString& filepath){}
 void MainModel::saveJSON(QString& filepath){}
 
@@ -33,6 +37,7 @@ void MainModel::addFrame(bool copyPrevious){
         frames.append(Frame(gridSize));
     }
 }
+
 void MainModel::deleteFrame(){
     if(frames.size() == 1){
         throw std::runtime_error("Cannot delete the only remaining frame");
@@ -60,15 +65,41 @@ void MainModel::deleteLayer(){
     frames[selectedFrame].deleteLayer();
 }
 
-void MainModel::changeBrushSize(unsigned int newBrushSize){}
-void MainModel::changeAnimationFPS(unsigned int newFPS){}
-void MainModel::changeSelectedColor(Pixel newColor){}
-void MainModel::mouseHovered(unsigned int xCoord, unsigned int yCoord){}
+void MainModel::changeBrushSize(unsigned int newBrushSize){
+    brushSize = newBrushSize;
+}
 
-void MainModel::paintPixels(unsigned int topLeftX, unsigned int topLeftY){}
-void MainModel::erasePixels(unsigned int topLeftX, unsigned int topLeftY){}
-void MainModel::bucketFill(unsigned int topLeftX, unsigned int topLeftY){}
-void MainModel::reflectVertical(){}
-void MainModel::reflectHorizontal(){}
-void MainModel::rotate90(){}
-void MainModel::setSelectedColorToPixel(unsigned int x, unsigned int y){}
+void MainModel::changeAnimationFPS(unsigned int newFPS){
+    animationFPS = newFPS;
+}
+
+void MainModel::changeSelectedColor(Pixel newColor){
+    selectedColor = newColor;
+}
+
+void MainModel::mouseHovered(unsigned int xCoord, unsigned int yCoord){
+    currentMouseX = xCoord;
+    currentMouseY = yCoord;
+}
+
+void MainModel::paintPixels(unsigned int topLeftX, unsigned int topLeftY){
+    frames[selectedFrame].paintPixels(topLeftX, topLeftY, selectedColor);
+}
+void MainModel::erasePixels(unsigned int topLeftX, unsigned int topLeftY){
+    frames[selectedFrame].paintPixels(topLeftX, topLeftY, Pixel(0,0,0,0));
+}
+void MainModel::bucketFill(unsigned int topLeftX, unsigned int topLeftY){
+    frames[selectedFrame].bucketFill(topLeftX, topLeftY, selectedColor);
+}
+void MainModel::reflectVertical(){
+    frames[selectedFrame].reflectVertical();
+}
+void MainModel::reflectHorizontal(){
+    frames[selectedFrame].reflectHorizontal();
+}
+void MainModel::rotate90(){
+    frames[selectedFrame].rotate90();
+}
+void MainModel::setSelectedColorToPixel(unsigned int x, unsigned int y){
+    selectedColor = frames[selectedFrame].getPixel(x, y);
+}
