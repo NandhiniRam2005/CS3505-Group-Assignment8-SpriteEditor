721ee3c3f490c773de1840fa75ef5411e2092d2d
jacobanderson <jacobAnderson931@gmail.com>
2025-03-21 11:18:24 -0600

    main model header file

--- mainmodel.h
+++ mainmodel.h
@@ -1,6 +1,12 @@
 #ifndef MAINMODEL_H
 #define MAINMODEL_H
 
+#include <QVector>
+#include <QString>
+
+#include "frame.h"
+#include "pixel.h"
+
 class MainModel : public QObject
 {
     Q_OBJECT
@@ -8,6 +14,48 @@ public:
     explicit MainModel(QObject *parent = nullptr);
 
 signals:
+    Pixel** newAnimationFrame();
+    Pixel** newDisplayImage();
+    Pixel** newSelectedFramePreview();
+    Pixel newSelectedColor();
+    bool loadJSONStatus();
+    bool saveJSONStatus();
+
+public slots:
+    void loadJSON(QString& filepath);
+    void saveJSON(QString& filepath);
+    void resize(unsigned int newSize);
+
+    void previousFrame();
+    void nextFrame();
+    void addFrame(bool copyPrevious);
+    void deleteFrame();
+    void changeLayer(unsigned int newLayer);
+    void addLayer(bool copyPrevious);
+    void deleteLayer();
+    void changeBrushSize(unsigned int newBrushSize);
+    void changeAnimationFPS(unsigned int newFPS);
+    void changeSelectedColor(Pixel newColor);
+    void mouseHovered(unsigned int xCoord, unsigned int yCoord);
+
+    void paintPixels(unsigned int topLeftX, unsigned int, topLeftY);
+    void erasePixels(unsigned int topLeftX, unsigned int, topLeftY);
+    void bucketFill(unsigned int topLeftX, unsigned int, topLeftY);
+    void reflectVertical();
+    void reflectHorizontal();
+    void rotate90();
+    void setSelectedColorToPixel(unsigned int x, unsigned int y);
+
+private:
+    QVector<Frame> frames;
+    size_t selectedFrame;
+    unsigned int brushSize;
+    unsigned int animationFPS;
+    Pixel selectedColor;
+    unsigned int gridWidth;
+    unsigned int gridHeight;
+    unsigned int currentMouseX;
+    unsigned int currentMouseY;
 };
 
 #endif // MAINMODEL_H
