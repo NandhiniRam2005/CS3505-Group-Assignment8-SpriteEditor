89783817fd8b254672ae2b793ff6c4a0d6392836
NandhiniRam <nandhini051@outlook.com>
2025-03-21 22:15:58 -0600

    Added EVENNN MORE header file comments: mainwindow (gave up halfway thru)

--- mainwindow.h
+++ mainwindow.h
@@ -1,3 +1,15 @@
+/*
+The header file for mainwindow.
+
+A class which represents represents the main window of the application.
+
+By Joel Rodriguez, Jacob Anderson,
+Adharsh Ramakrishnan, Nandhini Ramanathan
+Jake Heairld & Joseph Hamilton
+
+March 21, 2025
+*/
+
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
@@ -18,6 +30,7 @@ enum Tool {
     Eraser,
     Eyedropper,
 };
+
 QT_BEGIN_NAMESPACE
 namespace Ui {
 class MainWindow;
@@ -29,22 +42,76 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
+    /**
+     * Constructor that Initializes the MainWindow with an optional parent widget
+     * @param parent - parent widget (default is nullptr)
+     */
     MainWindow(QWidget *parent = nullptr);
+
+    /**
+     * Destructor for the mainwindow class.
+     */
     ~MainWindow();
+
 public slots:
-    void displayColorChange(Pixel p);
+    /**
+     * Slot to update the display color when the color changes
+     * @param pixel - The pixel with the new color
+     */
+    void displayColorChange(Pixel pixel);
+
+    /**
+     * Slot to update the load status on the UI
+     * @param status - The loading status
+     */
     void displayLoadStatus(bool status);
+
+    /**
+     * Slot to update the save status on the UI
+     * @param status - saving status
+     */
     void displaySaveStatus(bool status);
 
 
 signals:
+    /**
+     * Signal to load file
+     * @param filepath - filepath to load from
+     */
     void loadFile(QString filepath);
+
+    /**
+     * Signal to save file
+     * @param filepath - filepath to save from
+     */
     void saveFile(QString filepath);
+
+    /**
+     * Signal to go to the previous frame
+     */
     void previousFrame();
+
+    /**
+     * Signal to go to the next frame
+     */
     void nextFrame();
+
+    /**
+     * Signal to go to add a frame
+     */
     void addFrame();
+
+    /**
+     * Signal to go to delete a frame
+     */
     void deleteFrame();
+
+    /**
+     * Signal for chnaging the current layer
+     * @param layerNumber - number/index of the layer to change
+     */
     void changeLayer(unsigned int layerNumber);
+
     void addLayer();
     void deleteLayer();
     void changeBrushSize(unsigned int brushSize);
@@ -58,8 +125,6 @@ signals:
     void reflectHorizontal();
     void rotate90();
     void setSelectedColortoPixel(unsigned int x, unsigned int y);
-
-
 
 private:
     QWidget mouseListener;
