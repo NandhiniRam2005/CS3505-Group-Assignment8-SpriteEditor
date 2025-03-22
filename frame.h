51fd597b9b30dded36919b063d4ed87e91d3cc45
NandhiniRam <nandhini051@outlook.com>
2025-03-21 22:05:06 -0600

    Added MORE header file comments: fixed up frame and mainwindow

--- frame.h
+++ frame.h
@@ -1,3 +1,16 @@
+/*
+The header file for frame.
+
+A class which represents a frame.
+It represents an image frame consisting of layers of pixels.
+
+By Joel Rodriguez, Jacob Anderson,
+Adharsh Ramakrishnan, Nandhini Ramanathan
+Jake Heairld & Joseph Hamilton
+
+March 21, 2025
+*/
+
 #ifndef FRAME_H
 #define FRAME_H
 
@@ -10,33 +23,142 @@ using std::vector;
 class Frame
 {
 public:
+    /**
+     * custom contrcutor thart creates a new Frame object with the given size.
+     * @param size - size of the frame (square).
+     */
     Frame(unsigned int size);
+
+    /**
+     * Destructor for the frame class.
+     */
     ~Frame();
+
+
+    /**
+     * Copy constructor for creating a copy of the Frame.
+     * @param other - Frame object to copy.
+     */
     Frame(const Frame& other);
+
+    /**
+     * Assignment operator for the Frame class.
+     * @param other - Frame object to assign.
+     */
     void operator=(Frame other);
 
+    /**
+     * Returns the image with all layers applied, updated only if image has changed.
+     * @return A pointer to the layered image.
+     */
     Pixel* getLayeredImage();
+
+    /**
+     * Returns the final rendered image with all layers combined.
+     * @return A pointer to the rendered image.
+     */
     Pixel* getRenderedImage();
 
+    /**
+     * Selects the specified layer by index and sets it as the active layer.
+     * Hides the current active layer and makes the new one active.
+     * @param index - index of the layer to select.
+     * @throws run time error if the index is out of bounds.
+     */
     void selectLayer(unsigned int index);
+
+    /**
+     * Deletes the current layer.
+     * @throws run time error if the layer is the last one in the frame.
+     */
     void deleteLayer();
+
+    /**
+     * Adds a new layer to the frame.
+     */
     void addLayer();
+
+    /**
+     * Resizes the frame and all its layers to the new size.
+     * @param newSize - new size for the frame and layers.
+     */
     void resize(unsigned int newSize);
 
+    /**
+     * Paints pixels within the specified rectangular region on the active layer with the given color.
+     * @param corner1 - top-left corner of the region to paint.
+     * @param corner2 - bottom-right corner of the region to paint.
+     * @param color - color to paint the region with.
+     */
     void paintPixels(QPoint corner1, QPoint corner2, const Pixel& color);
+
+    /**
+     * Bucket fill starting from the specified coordinate position, filling all connected
+     * pixels with the specified color.
+     * @param x - x-coordinate of the starting position.
+     * @param y - y-coordinate of the starting position.
+     * @param color - color to fill the region with.
+     */
     void bucketFill(unsigned int x, unsigned int y, const Pixel& color);
+
+    /**
+     * Gets the pixel at the specified coordinates in the active layer.
+     * @param x - x-coordinate of the pixel to get.
+     * @param y - y-coordinate of the pixel to get.
+     * @return pixel at the given coordinates.
+     */
     Pixel getPixel(unsigned int x, unsigned int y) const;
+
+    /**
+     * Reflects the active layer vertically.
+     */
     void reflectVertical();
+
+    /**
+     * Reflects the active layer horizontally.
+     */
     void reflectHorizontal();
+
+
+    /**
+     * Rotates the active layer by 90 degrees clockwise.
+     */
     void rotate90();
 
 private:
+    /**
+     * Renders the layered image and final rendered image based on the current state.
+     */
     void renderImages();
+
+    /**
+     * vector of layers in the frame.
+     */
     QVector<Layer> layers;
+
+    /**
+     * index of the currently active layer in teh farme.
+     */
     unsigned int activeLayer;
+
+    /**
+     * size of the frame.
+     */
     unsigned int size;
+
+    /**
+     * The image (array of pixels) with all the layers applied.
+     */
     Pixel* layeredImage;
+
+    /**
+     * final rendered image as an array of pixels
+     */
     Pixel* renderedImage;
+
+    /**
+     * Flag to indicate if the image has changed and needs to be rerendered.
+     */
     bool imageChanged;
 };
 
--- mainmodel.h
+++ mainmodel.h
@@ -1,3 +1,17 @@
+/*
+The header file for mainmodel.
+
+A class which represents the model for the sprite editor. It manages frames, layers,
+brush size, animation speed, and pixel manipulations. The model sends updated pixel grids to
+the display through signals and handles user interactions with frames and layers.
+
+By Joel Rodriguez, Jacob Anderson,
+Adharsh Ramakrishnan, Nandhini Ramanathan
+Jake Heairld & Joseph Hamilton
+
+March 21, 2025
+*/
+
 #ifndef MAINMODEL_H
 #define MAINMODEL_H
 #include <QObject>
@@ -12,51 +26,228 @@ class MainModel : public QObject
 {
     Q_OBJECT
 public:
+
+    /**
+     * custom constructor for MainModel class.
+     * Initializes the model with default values and parent object.
+     * @param parent - parent QObject with default nullptr.
+     */
     explicit MainModel(QObject *parent = nullptr);
 
 signals:
+    /**
+     * Signal emitted when a new animation frame is available.
+     * @param pixelGrid - Pointer to the new pixel grid of the animation frame.
+     */
     void newAnimationFrame(const Pixel* pixelGrid);
+
+    /**
+     * Signal emitted when a new display image is available.
+     * @param pixelGrid - Pointer to the new pixel grid for display.
+     */
     void newDisplayImage(const Pixel* pixelGrid);
+
+    /**
+     * Signal emitted when a new preview of the selected frame is available.
+     * @param pixelGrid - Pointer to the new pixel grid for the selected frame preview.
+     */
     void newSelectedFramePreview(const Pixel* pixelGrid);
+
+    /**
+     * Signal emitted when a new color is selected.
+     * @param color - new selected color.
+     */
     void newSelectedColor(Pixel color);
+
+
+    /**
+     * Signal emitted to indicate the whether loading was successful or not.
+     * @param status - True if loading was successful, false otherwise.
+     */
     void loadJSONStatus(bool status);
+
+    /**
+     * Signal emitted to indicate the whether saving was successful or not.
+     * @param status - True if saving was successful, false otherwise.
+     */
     void saveJSONStatus(bool status);
 
 public slots:
+    /**
+     * Loads a JSON file containing canvas' data.
+     * @param filepath - path to the JSON file to load.
+     */
     void loadJSON(QString& filepath);
+
+    /**
+     * Saves the current canvas' data to a JSON file.
+     * @param filepath - path to the JSON file where data will be saved.
+     */
     void saveJSON(QString& filepath);
+
+    /**
+     * Resizes the grid size for all frames.
+     * @param newSize The new size for the grid.
+     */
     void resize(unsigned int newSize);
 
+    /**
+     * Switches to the previous frame and updates the display.
+     */
     void previousFrame();
+
+    /**
+     * Switches to the next frame and updates the display.
+     */
     void nextFrame();
+
+    /**
+     * Adds a new frame. Also copies the previous frame’s data for duplication if wanted.
+     * @param copyPrevious - if true, the previous frame is copied, otherwise, a new empty frame is added.
+     */
     void addFrame(bool copyPrevious);
+
+    /**
+     * Deletes the current frame.
+     * @throws run time error if this is the only frame.
+     */
     void deleteFrame();
+
+    /**
+     * Changes the active layer in the selected frame.
+     * @param layer - index of the new active layer.
+     * @throws run time error if the layer index is invalid.
+     */
     void changeLayer(unsigned int layer);
+
+    /**
+     * Adds a new layer to the selected frame.
+     */
     void addLayer();
+
+    /**
+     * Deletes the active layer in the selected frame.
+     */
     void deleteLayer();
+
+    /**
+     * Changes the size of the brush used for painting and erasing.
+     * @param newBrushSize - new brush size.
+     */
     void changeBrushSize(unsigned int newBrushSize);
+
+
+    /**
+     * Changes the frames per second for the animation.
+     * @param newFPS - new FPS value.
+     */
     void changeAnimationFPS(unsigned int newFPS);
+
+    /**
+     * Changes the currently selected color.
+     * @param newColor - new selected color.
+     */
     void changeSelectedColor(Pixel newColor);
+
+    /**
+     * Updates the coordinates of the mouse hover position.
+     * @param xCoord - new x-coordinate of the mouse.
+     * @param yCoord - new y-coordinate of the mouse.
+     */
     void mouseHovered(unsigned int xCoord, unsigned int yCoord);
 
+    /**
+     * Paints pixels on the selected frame at the specified location.
+     * @param topLeftX - x-coordinate of the top-left corner of the region to paint.
+     * @param topLeftY - y-coordinate of the top-left corner of the region to paint.
+     */
     void paintPixels(unsigned int topLeftX, unsigned int topLeftY);
+
+    /**
+     * Erases pixels on the selected frame at the specified location.
+     * @param topLeftX - x-coordinate of the top-left corner of the region to erase.
+     * @param topLeftY - y-coordinate of the top-left corner of the region to erase.
+     */
     void erasePixels(unsigned int topLeftX, unsigned int topLeftY);
+
+    /**
+     * Fills a region with the selected color using the bucket fill tool.
+     * @param topLeftX - x-coordinate of the region to start filling.
+     * @param topLeftY - y-coordinate of the region to start filling.
+     */
     void bucketFill(unsigned int topLeftX, unsigned int topLeftY);
+
+    /**
+     * Reflects the selected frame vertically.
+     */
     void reflectVertical();
+
+    /**
+     * Reflects the selected frame horizontally.
+     */
     void reflectHorizontal();
+
+    /**
+     * Rotates the selected frame by 90 degrees clockwise.
+     */
     void rotate90();
+
+    /**
+     * Sets the selected color to the color of a pixel at the specified coordinates.
+     * @param x - x-coordinate of the pixel.
+     * @param y - y-coordinate of the pixel.
+     */
     void setSelectedColorToPixel(unsigned int x, unsigned int y);
 
 private:
+    /**
+     * Sends the current display image to the display.
+     */
     void sendDisplayImage();
+
+    /**
+     * vector collection of frames in the animation.
+     */
     QVector<Frame> frames;
+
+    /**
+     * index of the currently selected frame
+     */
     qsizetype selectedFrame;
+
+    /**
+     * current brish size for painting and erasing
+     */
     int brushSize;
+
+    /**
+     * frames per second of the animation
+     */
     unsigned int animationFPS;
+
+    /**
+     * currently selected color for painting
+     */
     Pixel selectedColor;
+
+    /**
+     * size of the grid (square - width and height)
+     */
     unsigned int gridSize;
+
+    /**
+     * x coordinate of the mouse position currently
+     */
     int currentMouseX;
+
+    /**
+     * y coordinate of the mouse position currently
+     */
     int currentMouseY;
+
+    /**
+     * alpha value for hovered pixels
+     */
     const unsigned char HOVERED_ALPHA = 200;
 };
 
