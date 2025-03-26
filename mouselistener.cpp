983299c56f621e0f4a4017bbe234a6420af6da6b
jakeheairld <jakeheairld1@gmail.com>
2025-03-26 10:06:34 -0600

    Added mouse move and release event for mouse dragging

--- mouselistener.cpp
+++ mouselistener.cpp
@@ -6,6 +6,18 @@ MouseListener::MouseListener(QWidget *parent)
 
 void MouseListener::mousePressEvent(QMouseEvent* event){
     QPoint screenCoords = event->pos();
-
     emit mouseClicked(screenCoords);
+    isMousePressed = true;
+}
+
+void MouseListener::mouseMoveEvent(QMouseEvent* event) {
+    if (isMousePressed) {
+        QPoint screenCoords = event->pos();
+        emit mouseMoved(screenCoords);
+    }
+}
+
+void MouseListener::mouseReleaseEvent(QMouseEvent* event) {
+    isMousePressed = false;
+    emit mouseReleased();
 }
