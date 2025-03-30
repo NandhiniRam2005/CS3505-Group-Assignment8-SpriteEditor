27333010e9eeea4a402e139d31a50a6f596f0790
Joel <joelrodriguez2023@outlook.com>
2025-03-30 12:36:48 -0600

    Documented all signals in main window

--- mainwindow.h
+++ mainwindow.h
@@ -132,7 +132,7 @@ signals:
     void nextFrame();
 
     /**
-     * Signal to go to add a frame
+     * Signal to go to add a frame to the model
      */
     void addFrame(bool copyPrevious);
 
@@ -147,19 +147,87 @@ signals:
      */
     void changeLayer(int layerNumber);
 
+    /**
+     * Signal to add a layer in the model.
+     */
     void addLayer();
 
+    /**
+     * Signal to delete a layer in the model at a certain index.
+     * @param layerIndex - the index at which the layer lives at, for example layer at index one is
+     *                    the bottom most layer
+     */
     void deleteLayer(int layerIndex);
+
+    /**
+     * Signal to change the size of the brush in the model.
+     *
+     * @param brushSize - the new brush size in pixel coordinates
+     */
     void changeBrushSize(unsigned int brushSize);
+
+    /**
+     * Signal to change the fps at which the animation frame show images in the model
+     *
+     * @param fps - the new fps
+     */
     void changeFps(unsigned int fps);
-    void changeColor(Pixel p);
+
+    /**
+     * Signals to the model that a new color has been chosen. Color is represented by pixels.
+     *
+     * @param color - The new color aquired from the view
+     */
+    void changeColor(Pixel color);
+
+    /**
+     * Signal that is able to send the current hover position of the mouse in widget coordinates
+     *
+     * @param x - The current x widget coordinate
+     * @param y - The current y widget coordinate
+     */
     void mouseHoverLocation(unsigned int x, unsigned int y);
+
+    /**
+     * Signal to the model that the image should be reflected vertically
+     */
     void reflectVertical();
+
+    /**
+     * Signal to the model that the image should be reflected horizontally
+     */
     void reflectHorizontal();
+
+    /**
+     * Signal to the model that the image should be rotated 90 degrees clockwise
+     */
     void rotate90();
+
+    /**
+     * Alerts the model to return the current grid size in pixel coordinates.
+     */
     void askGridSize();
+
+    /**
+     * Singals that the canvas has changed in size (8x8, 16x16, 32x32, or 64x64)
+     *
+     * @param newSize - The new size of the canvas
+     */
     void resize(unsigned int newSize);
+
+    /**
+     * Signals that a pixel (in pixels coords) has been clicked.
+     *
+     * @param xCoord - The x coordinate that was clicked on.
+     * @param yCoord - The y coordinate that was clicked on.
+     */
     void pixelClicked(unsigned int xCoord, unsigned int yCoord);
+
+    /**
+     * Singals that a new tool has been selected.
+     *
+     * @param newTool - A tool enum which represents either an eyedropper, paintbucket, brush, or eraser.
+     */
     void toolSelected(Tool newTool);
 
 private:
